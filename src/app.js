b5123298ada7338fbf907d212587c371eb5057d6
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-02 14:51:00 +0530

    created rest appis

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
-console.log("Starting a new project");
+const express=require("express")
 
+const app=express();
+
+app.use("/test",(req,res)=>{
+    res.send("test routing");
+});
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"sri",lastname:"datthu"})
+})
+app.post("/user",(req,res)=>{
+    res.send("posted sucessfully")
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("patch sucesss")
+})
+app.delete("/user",(req,res)=>{
+    res.send("delete sucesss")
+})
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+    
+});
