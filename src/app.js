140df59cf3f47c0ad1f62f8dfbcd2b8fe4791458
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-03 16:16:25 +0530

    mongodb get patch delete post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const  connectDB=require("./config/database")
 const app=express();
 const User=require("./models/user")
 app.use(express.json())
+
 app.post("/signup",async (req,res)=>{
     const user=new User(req.body);
     try{
@@ -12,6 +13,32 @@ app.post("/signup",async (req,res)=>{
 res.status(400).send("errror saving")
     }
 });
+app.post("/user",async (req,res)=>{
+    const mail=req.body.emailId;
+    console.log(req.body)
+    const users=await User.find({emailId:mail});
+    res.send(users)
+    // res.send("hello")
+})
+app.delete("/user",async (req,res)=>{
+    const id=req.body.userId;
+    const users=await User.findByIdAndDelete({_id:id});
+    res.send("user deleted")
+    // res.send("hello")
+})
+app.patch("/user",async (req,res)=>{
+    const id=req.body.userId;
+    const data=req.body;
+    const users=await User.findByIdAndUpdate({_id:id},data);
+    res.send("user updated")
+    // res.send("hello")
+})
+app.get("/feed",async (req,res)=>{
+    
+    const users=await User.find({});
+    res.send(users)
+    // res.send("hello")
+})
 connectDB().
     then(()=>{
         console.log("database connected");
