9c996c03bebd82af7c57b0dd07f70d514e3ca212
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-03 11:57:15 +0530

    middleware and next

--- src/app.js
+++ src/app.js
@@ -2,23 +2,19 @@ const express=require("express")
 
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send("test routing");
+app.use("/test",[(req,res,next)=>{
+    console.log("route 1");
+    
+
+    next();
+},(req,res,next)=>{
+    console.log("route 2")
+next()
+}],(req,res)=>{
+    res.send("test 3")
 });
 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"sri",lastname:"datthu"})
-})
-app.post("/user",(req,res)=>{
-    res.send("posted sucessfully")
-})
 
-app.patch("/user",(req,res)=>{
-    res.send("patch sucesss")
-})
-app.delete("/user",(req,res)=>{
-    res.send("delete sucesss")
-})
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
     
