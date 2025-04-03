fe2c73df3f64b0390c770b44452e485d949f2469
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-04-03 14:25:16 +0530

    dynamic data express.json middleware

--- src/app.js
+++ src/app.js
@@ -2,14 +2,9 @@ const express=require("express")
 const  connectDB=require("./config/database")
 const app=express();
 const User=require("./models/user")
-
+app.use(express.json())
 app.post("/signup",async (req,res)=>{
-    const user=new User({
-        firstName:"virat",
-        lastName:"kohli",
-        emailId:"virat@gmail.com",
-        password:"virat@123",
-    });
+    const user=new User(req.body);
     try{
         await user.save();
         res.send("user saved successfully");
